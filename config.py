9248e59c4fc980694151ea9b620a509b233b1bc8
jufy111 <40817638+jufy111@users.noreply.github.com>
2023-08-13 18:43:56 +1000

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+# InfluxDB Database Details
+token = "yourtokenhere"
+org = "yourorghere"
+bucket = "yourbuckethere"
+influxDB_url = "http://10.10.10.10:8086"  #your influxdb url here
+
+# EcoWitt devices details
+sample_time = 10  # in seconds
+GW2000_ip_address = '10.10.10.100' #your gateway IP here
+location = 'yourlocation'  #tag for influxDB change to 'home' or whatever you want.
